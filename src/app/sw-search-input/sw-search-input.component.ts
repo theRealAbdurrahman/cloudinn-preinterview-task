e5cc7112ad1d9193388ecf53a4dd3b15b8936631
Abdurrrahman <abdurrahman.maged@gmail.com>
2018-07-15 05:51:55 +0200

    * Did some refactoring and moved the search component to the search input component

--- src/app/sw-search-input/sw-search-input.component.ts
+++ src/app/sw-search-input/sw-search-input.component.ts
@@ -1,3 +1,5 @@
+import { SwAPIService } from './../sw-api.service';
+import { SortablejsOptions } from 'angular-sortablejs/dist';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sw-search-input.component.css']
 })
 export class SwSearchInputComponent implements OnInit {
+  searchStr: any = "";
+  searchResults = [];
 
-  constructor() { }
+  options: SortablejsOptions;
+  constructor(private swAPI: SwAPIService) { }
 
   ngOnInit() {
+    this.options = {
+      animation: 300,
+      onUpdate: (e) => {
+        // console.log(this.searchResults);
+      }
+    }
   }
+
+  getPeople() {
+    this.swAPI.getPeople(this.searchStr).subscribe(
+      (res) => {
+        // console.log(res);
+        this.searchResults = res['results'];
+      },
+      err => console.log(err),
+      () => {
+        this.getHomeWorld();
+
+      }
+
+    )
+  }
+  getHomeWorld() {
+    let homeworld = [];
+    this.searchResults.forEach(result => {
+
+      let homeworldUrl = result['homeworld'];
+
+      this.swAPI.getPlanet(homeworldUrl).subscribe(
+        res => {
+          homeworld.push(res['name'])
+
+
+        },
+        err => console.log(err),
+        () => {
+          this.searchResults.map((searchResult, i) => {
+            // console.log("search result", searchResult);
+
+            searchResult['homeworld'] = homeworld[i];
+          })
+        }
+
+      )
+
+    });
+
+  }
+
 
 }

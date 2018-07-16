d09df8d3812b17bd4de35f64a003039cb3ae08e9
Abdurrrahman <abdurrahman.maged@gmail.com>
2018-07-16 12:36:53 +0200

    ** my first test ever ** turned off source maps

--- src/app/sw-api.service.spec.ts
+++ src/app/sw-api.service.spec.ts
@@ -3,13 +3,26 @@ import { TestBed, inject } from '@angular/core/testing';
 import { SwAPIService } from './sw-api.service';
 
 describe('SwAPIService', () => {
+  let httpClientSpy: { get: jasmine.Spy };
+  let swService: SwAPIService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [SwAPIService]
     });
+    swService = TestBed.get(SwAPIService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    swService = new SwAPIService(<any>httpClientSpy);
+
   });
 
   it('should be created', inject([SwAPIService], (service: SwAPIService) => {
     expect(service).toBeTruthy();
   }));
+  it('#getPeople() should return observable value', (done: DoneFn) => {
+
+    swService.getPeople("luke").subscribe(value => {
+      expect(value).toBe('observable value');
+      done();
+    });
+  })
 });
